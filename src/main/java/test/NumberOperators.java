a5e2d2cdd09c7f55043ea723c4d14d41cbbea6f6
DHolda <damian.holda@patsystems.com>
2014-01-24 13:11:49 +0000

    update to shifts

--- src/main/java/test/NumberOperators.java
+++ src/main/java/test/NumberOperators.java
@@ -1,31 +1,28 @@
 package test;
 
-import org.junit.Test;
-
 public class NumberOperators {
 
-	@Test
-	public void main() {
+	
+	public static void main(String[] args) {
 		byte negativeByte = -8;
-		System.out.println("negativeByte: "+negativeByte);
+		System.out.println("negativeByte: int: "+negativeByte);
 		byte positiveByte = 8;
 		System.out.println("positiveByte: "+positiveByte);
-		
+		//arithmetic
 		int shiftLeftNegativeByte = negativeByte << 1;
 		System.out.println("shiftLeft NegativeByte: "+shiftLeftNegativeByte);
 		int shiftLeftPositiveByte = positiveByte << 1;
 		System.out.println("shiftLeft PositiveByte: "+shiftLeftPositiveByte);
 		
-		int shiftRightNegativeByte = negativeByte >>> 1;
+		int shiftRightNegativeByte = negativeByte >> 1;
 		System.out.println("shiftRight NegativeByte: "+shiftRightNegativeByte);
-		int shiftRightPositiveByte = positiveByte >>> 1;
+		int shiftRightPositiveByte = positiveByte >> 1;
 		System.out.println("shiftRight PositiveByte: "+shiftRightPositiveByte);
-		
-		int shiftRightArtNegativeByte = negativeByte >> 1;
-		System.out.println("shiftRightArt NegativeByte: "+shiftRightArtNegativeByte);
-		int shiftRightArtPositiveByte = positiveByte >> 1;
-		System.out.println("shiftRightArt PositiveByte: "+shiftRightArtPositiveByte);
-		
+		//logical
+		int shiftRightLogicalNegativeByte = negativeByte >>> 1;
+		System.out.println("shiftRightLogical NegativeByte: "+shiftRightLogicalNegativeByte);
+		int shiftRightLogicalPositiveByte = positiveByte >>> 1;
+		System.out.println("shiftRightLogical PositiveByte: "+shiftRightLogicalPositiveByte);		
 
 	}
 
